180a2a48ebd8262e0c197e754202f8273519b0f0
Srishti Arora <srishti.arora1909@gmail.com>
2025-10-03 15:27:44 -0400

    Update ALLOWED_HOSTS for Elastic Beanstalk

--- myapp/settings.py
+++ myapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t&$$p92co^1l78l)(5g^$eyn5oq(p=@4juvlrynj4##zv5)0^a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.elasticbeanstalk.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
